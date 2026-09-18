7a5aea8bb3f046d54df0a5dff58518ec1fa18cfc
piklowski <kmp8@alu.ua.es>
2026-09-18 16:48:48 +0200

    conseguido el avance para realizar la fase 1.

--- practica1/expertSystem.py
+++ practica1/expertSystem.py
@@ -34,13 +34,14 @@ class ExpertSystem:
         #obtenemos los datos del robot en el instante en el que vamos a tomar la decision. 
         x_robot = poseRobot[0]
         y_robot = poseRobot[1]
-        angulo_robot = poseRobot[2]
+        angulo_robot = math.radians(poseRobot[2])
 
         #----------------------------------------------------------------------------------------
 
         '''
         obtenemos el tipo de objetivo con el que se enfrenta el robot. 
         '''
+
         if self.objetivoActual.getType() == 1 and self.fase==1:
             x_objetivo, y_objetivo = self.objetivoActual.getInicio()
         elif self.objetivoActual.getType() == 1 and self.fase==2:
@@ -68,7 +69,7 @@ class ExpertSystem:
 
         dif_angulo = angulo_referencia - angulo_robot
         error_angulo = math.atan2(math.sin(dif_angulo), math.cos(dif_angulo))
-        margen = 0.08
+        margen = 0.1
 
         #----------------------------------------------------------------------------------------
 
@@ -76,11 +77,30 @@ class ExpertSystem:
         #REGLAS DE ESTADO. 
         #=========================
 
+        if distancia <= self.umbral:
+            self.fase+=1
+
+        if self.fase==2:
+            return (0.0,0.0)
+
 
         #=========================
         #REGLAS DE CONTROL (VELOCIDADES). 
         #=========================
         
+        if abs(error_angulo) < margen:
+            v_lineal = 3.0
+            v_angular = 0.0
+            
+        else:
+            v_lineal = 0.0
+            if error_angulo > 0:
+                v_angular = 0.5
+            else:
+                v_angular = -0.5
+
+
+
 
         #tenemos que devolover la tupla con las velocidades que queremos que tome el robot. 
         return (v_lineal, v_angular)
